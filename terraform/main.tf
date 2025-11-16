065d4052ddbfb3b2434a7ec48ca8fb1b4e88e35f
Olusegun <shegoj@yahoo.com>
2025-11-16 21:47:47 +0000

    adding terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
-# place holder for Terraform
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+resource "aws_instance" "nginx-node" {
+  ami                    = "ami-038494630aa32ef56"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-060ba13bd6800a0db"
+  vpc_security_group_ids = ["sg-090804d4ff518079d"]
+  key_name               = "MasterClass2025"
+
+  tags = {
+    Name = "terraform-nginx-node"
+  }
+}
