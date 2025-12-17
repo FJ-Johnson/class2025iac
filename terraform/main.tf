c1de4e64ba32ba0e4cfea70b6a878818610f2e98
Folorunson <FOLJohnson12@gmail.com>
2025-12-17 04:05:42 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket  = "techbleat-cicd-state-bucket"
-    key     = "envs/dev/terraform.tfstate"
-    region  = "eu-west-1"
-    encrypt = true
+  
 
   }
   required_version = ">= 1.6.0"
